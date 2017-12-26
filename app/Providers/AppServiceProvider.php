46a60fcb891190c46d5b65755a7f200e87d60dd6
ONEMYX <32790438+ONEMYX@users.noreply.github.com>
2017-12-26 16:01:50 +0800

    shiping5

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //定义数据库String 的默认长度
+        Schema::defaultStringLength(250);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
