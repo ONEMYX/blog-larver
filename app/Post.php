f7affcb3e9f4a6e19493545c87f6ab41f76d041a
ONEMYX <32790438+ONEMYX@users.noreply.github.com>
2018-01-08 15:32:18 +0800

    第八章赞模块

--- app/Post.php
+++ app/Post.php
@@ -7,12 +7,24 @@ use App\Model;
 class Post extends Model
 {
     // 关联用户
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo('App\User');
     }
     // 评论模型
     public function comments()
     {
         return $this->hasMany('App\Comment')->orderBy('created_at','desc');
+    }
+
+    //赞和用户进行关联
+    public function zan($user_id)
+    {
+        return $this->hasOne(\App\Zan::class)->where('user_id',$user_id);
+    }
+    //文章的所有赞
+    public function zans()
+    {
+        return $this->hasMany(\App\Zan::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -52,4 +52,10 @@ Route::get('/posts/{post}/delete','\App\Http\Controllers\PostController@delete')
 Route::post('/posts/img/upload','\App\Http\Controllers\PostController@imageUpload');
 //提交评论
 Route::post('/posts/{post}/comment','\App\Http\Controllers\PostController@comment');
+//点赞
+Route::get('/posts/{post}/zan','\App\Http\Controllers\PostController@zan');
+//取消赞
+Route::get('/posts/{post}/unzan','\App\Http\Controllers\PostController@unzan');
+
+
 
